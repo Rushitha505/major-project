06b7683789e02b31a14f0fe3b0f4aeb71bd7919b
Rushitha505 <23891a0505@gmail.com>
2025-10-15 23:31:02 +0530

    Initial commit for Major Project EB app

---
+++ app.js
@@ -0,0 +1,8 @@
+ECHO is on.
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from AWS Elastic Beanstalk!'));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
