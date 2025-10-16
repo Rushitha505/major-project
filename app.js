930fb3c56306bd178352c2631e17f0adea569a95
Rushitha505 <23891a0505@gmail.com>
2025-10-16 23:24:19 +0530

    Initial commit of major-project

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // EB assigns the port
 
-// Simple route
 app.get('/', (req, res) => {
-  res.send('Hello from AWS Elastic Beanstalk!');
+  res.send('Hello from Elastic Beanstalk!');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
